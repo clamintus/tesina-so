7ce3513e45bc9762130348566b3c9c7056abc98f
clamintus <44071820+clamintus@users.noreply.github.com>
2026-01-13 19:58:25 +0100

    Irrobustito script per aggiunta utenti

--- useradd.sh
+++ useradd.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [ -z "$1" -o -z "$2" -o -z "$3" ]; then
+if [ -z "$1" -o -z "$2" -o -z "$3" ] || [ "$3" != "0" -a "$3" != "1" ]; then
 	echo "Sintassi: ./useradd.sh <username> <password> <is_admin>"
+	exit 1
+fi
+if [ $(expr length "$1") -gt 255 -o $(expr length "$2") -gt 255 ]; then
+	echo "Username o password troppo lunga (max 255 caratteri)"
 	exit 1
 fi
 
